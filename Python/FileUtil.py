c3cf523f91f7e6e285d6aafeb7727542781e08eb
chenwu <chenwu@dartou.com>
2018-03-24 21:58:17 +0800

    Add

--- Python/FileUtil.py
+++ Python/FileUtil.py
@@ -1,3 +1,4 @@
+#coding=utf8
 import shutil
 import os
 
@@ -9,26 +10,86 @@ class FileUtil:
 	TYPE_UNKNOW = 0
 
 	@classmethod
-	def deletePath(cls, path):
-		os.remove(path)
+	def delete(cls, path):
+		if not cls.exist(path):
+			return False
+		typePath = cls.type(path)
+		if typePath == 0:
+			return False
+
+		if typePath == 1:
+			return os.remove(path)
+		else:
+			return shutil.rmtree(path)
 
 	@classmethod
-	def typePath(cls, path):
-		typePath = TYPE_UNKNOW
+	def exist(cls, path):
+		return os.path.exists(path)
+
+	@classmethod
+	def join(cls, root, *dirs):
+	    for item in dirs:
+	        path = os.path.join(root, item)
+	    return path
+
+	@classmethod
+	def type(cls, path):
 		if os.path.isdir(path):
-			typePath = TYPE_DIR
+			return FileUtil.TYPE_DIR
 		elif os.path.isfile(path):
-			typePath = TYPE_FILE
-		return typePath
+			return FileUtil.TYPE_FILE
+		else:
+			return FileUtil.TYPE_UNKNOW
+
+	@classmethod
+	def mk(cls, path):
+		if not cls.exist(path):
+			os.mkdir(path)
+
+	#ab+ add wb+ cover
+	@classmethod
+	def write(cls, path, data, model):
+		outFp = open(path, model)
+		outFp.write(data)
+		outFp.close()
+
+	@classmethod
+	def remove(cls, srcPath, desPath):
+		cls.copy(srcPath ,desPath)
+		cls.delete(srcPath)
+
+	@classmethod
+	def copy(cls, srcPath, desPath):
+		if not cls.exist(srcPath):
+			return False
+		typePath = cls.type(srcPath)
+		if typePath == 1:
+			return shutil.copy(srcPath, desPath)
+		else:
+			return shutil.copytree(srcPath, desPath)
 	
-	def removePath(cls, srcPath, desPath):
-		pass
+	@classmethod
+	def list(cls, path):
+		array = []
+		for item in os.listdir(path):
+			itemPath = cls.join(path, item)
+			if os.path.isfile(itemPath):
+				array.append(itemPath)
+			else:
+				array.append(itemPath)
+				array = array + cls.list(itemPath)
+		return array
 
-	def copyPath(cls, srcPath, desPath):
-		pass
+	@classmethod
+	def cmd(cls, cmd):
+		output = os.popen(cmd)
+		return output.read()
 
-	def copyPath(cls, srcPath, desPath):
-		pass
-
-
-FileUtil.deletePath(path + "1")
+#typePath = FileUtil.typePath(path + "1")
+FileUtil.write(path + "1/3.txt", "123", "ab+	")
+FileUtil.write(path + "3.txt", "456", "ab+")
+FileUtil.mk(path + "1")
+FileUtil.remove(path + "1.txt",path + "2.txt")
+print(FileUtil.list(path))
+print(FileUtil.cmd("ifconfig"))
+#FileUtil.delete(path + "1")
